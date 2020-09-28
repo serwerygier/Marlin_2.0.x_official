d8d70a73f3f5b0580540bdd245426e4b310200af
Malderin <52313714+Malderin@users.noreply.github.com>
2020-09-28 06:56:42 +0300

    bed pid correct

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -542,24 +542,18 @@
 #if ENABLED(PIDTEMPBED)
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
-
-<<<<<<< HEAD
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
+
   //#define DEFAULT_bedKp 10.00
   //#define DEFAULT_bedKi .023
   //#define DEFAULT_bedKd 305.4
 #define DEFAULT_bedKp 63.10
 #define DEFAULT_bedKi 11.93
 #define DEFAULT_bedKd 222.57
-=======
+
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
->>>>>>> upstream/bugfix-2.0.x
-
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
 

fcc3830f43e9b83537b3a186f075858882a14f4e
Malderin <52313714+Malderin@users.noreply.github.com>
2020-10-09 10:42:47 +0300

    Test custom logo

--- buildroot/share/PlatformIO/scripts/download_mks_assets.py
+++ buildroot/share/PlatformIO/scripts/download_mks_assets.py
@@ -6,12 +6,14 @@ import tempfile
 import shutil
 
 url = "https://github.com/makerbase-mks/Mks-Robin-Nano-Marlin2.0-Firmware/archive/master.zip"
+url1 = "https://github.com/Malderin/Marlin_2.0.x_official/tree/Marlin2.0.7_bugfix/Download/master1.zip"
 zip_path = os.path.join(env.Dictionary("PROJECT_LIBDEPS_DIR"), "mks-assets.zip")
 assets_path = os.path.join(env.Dictionary("PROJECT_BUILD_DIR"), env.Dictionary("PIOENV"), "assets")
 
 def download_mks_assets():
 	print("Downloading MKS Assets")
 	r = requests.get(url, stream=True)
+	r1 = requests.get(url1, stream=True)
 	# the user may have a very clean workspace,
 	# so create the PROJECT_LIBDEPS_DIR directory if not exits
 	if os.path.exists(env.Dictionary("PROJECT_LIBDEPS_DIR")) == False:
@@ -39,8 +41,30 @@ def copy_mks_assets():
 		shutil.copy(os.path.join(output_path, base_path, 'Firmware', 'mks_pic', filename), assets_path)
 	shutil.rmtree(output_path, ignore_errors=True)
 
+def copy_mks_assets1():
+	print("Copying MKS Assets logo")
+	output_path = tempfile.mkdtemp()
+	zip_obj = zipfile.ZipFile(zip_path, 'r1')
+	zip_obj.extractall(output_path)
+	zip_obj.close()
+	if os.path.exists(assets_path) == True and os.path.isdir(assets_path) == False:
+		os.unlink(assets_path)
+	if os.path.exists(assets_path) == False:
+		os.mkdir(assets_path)
+	base_path = ''
+	for filename in os.listdir(output_path):
+		base_path = filename
+	for filename in os.listdir(os.path.join(output_path, base_path, 'Firmware', 'mks_font')):
+		shutil.copy(os.path.join(output_path, base_path, 'Firmware', 'mks_font', filename), assets_path)
+	for filename in os.listdir(os.path.join(output_path, base_path, 'Firmware', 'mks_pic')):
+		shutil.copy(os.path.join(output_path, base_path, 'Firmware', 'mks_pic', filename), assets_path)
+	shutil.rmtree(output_path, ignore_errors=True)
+
+
 if os.path.exists(zip_path) == False:
 	download_mks_assets()
+	download_mks_assets1()
 
 if os.path.exists(assets_path) == False:
 	copy_mks_assets()
+	copy_mks_assets1()

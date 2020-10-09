c57c41acca472a4d8d452ce1ccf81237282c42f1
Malderin <52313714+Malderin@users.noreply.github.com>
2020-10-09 13:55:47 +0300

    add

--- buildroot/share/PlatformIO/scripts/download_mks_assets.py
+++ buildroot/share/PlatformIO/scripts/download_mks_assets.py
@@ -6,14 +6,12 @@ import tempfile
 import shutil
 
 url = "https://github.com/makerbase-mks/Mks-Robin-Nano-Marlin2.0-Firmware/archive/master.zip"
-url1 = "https://github.com/Malderin/Marlin_2.0.x_official/tree/Marlin2.0.7_bugfix/Download/master1.zip"
 zip_path = os.path.join(env.Dictionary("PROJECT_LIBDEPS_DIR"), "mks-assets.zip")
 assets_path = os.path.join(env.Dictionary("PROJECT_BUILD_DIR"), env.Dictionary("PIOENV"), "assets")
 
 def download_mks_assets():
 	print("Downloading MKS Assets")
 	r = requests.get(url, stream=True)
-	r1 = requests.get(url1, stream=True)
 	# the user may have a very clean workspace,
 	# so create the PROJECT_LIBDEPS_DIR directory if not exits
 	if os.path.exists(env.Dictionary("PROJECT_LIBDEPS_DIR")) == False:
@@ -41,30 +39,49 @@ def copy_mks_assets():
 		shutil.copy(os.path.join(output_path, base_path, 'Firmware', 'mks_pic', filename), assets_path)
 	shutil.rmtree(output_path, ignore_errors=True)
 
+url1 = "https://github.com/Malderin/Marlin_2.0.x_official/raw/Marlin2.0.7_bugfix/Download/master1.zip"
+zip_path1 = os.path.join(env.Dictionary("PROJECT_LIBDEPS_DIR"), "mks-assets1.zip")
+assets_path1 = os.path.join(env.Dictionary("PROJECT_BUILD_DIR"), env.Dictionary("PIOENV"), "assets")
+
+
+def download_mks_assets1():
+	print("Downloading MKS Assets logo")
+	r = requests.get(url1, stream=True)
+	# the user may have a very clean workspace,
+	# so create the PROJECT_LIBDEPS_DIR directory if not exits
+	if os.path.exists(env.Dictionary("PROJECT_LIBDEPS_DIR")) == False:
+		os.mkdir(env.Dictionary("PROJECT_LIBDEPS_DIR"))
+	with open(zip_path1, 'wb') as fd:
+		for chunk in r.iter_content(chunk_size=128):
+			fd.write(chunk)
+
 def copy_mks_assets1():
 	print("Copying MKS Assets logo")
 	output_path = tempfile.mkdtemp()
-	zip_obj = zipfile.ZipFile(zip_path, 'r1')
+	zip_obj = zipfile.ZipFile(zip_path1, 'r')
 	zip_obj.extractall(output_path)
 	zip_obj.close()
-	if os.path.exists(assets_path) == True and os.path.isdir(assets_path) == False:
-		os.unlink(assets_path)
-	if os.path.exists(assets_path) == False:
-		os.mkdir(assets_path)
+	if os.path.exists(assets_path1) == True and os.path.isdir(assets_path1) == False:
+		os.unlink(assets_path1)
+	if os.path.exists(assets_path1) == False:
+		os.mkdir(assets_path1)
 	base_path = ''
 	for filename in os.listdir(output_path):
 		base_path = filename
 	for filename in os.listdir(os.path.join(output_path, base_path, 'Firmware', 'mks_font')):
-		shutil.copy(os.path.join(output_path, base_path, 'Firmware', 'mks_font', filename), assets_path)
+		shutil.copy(os.path.join(output_path, base_path, 'Firmware', 'mks_font', filename), assets_path1)
 	for filename in os.listdir(os.path.join(output_path, base_path, 'Firmware', 'mks_pic')):
-		shutil.copy(os.path.join(output_path, base_path, 'Firmware', 'mks_pic', filename), assets_path)
+		shutil.copy(os.path.join(output_path, base_path, 'Firmware', 'mks_pic', filename), assets_path1)
 	shutil.rmtree(output_path, ignore_errors=True)
 
+if os.path.exists(zip_path1) == False:
+	download_mks_assets1()
+
+if os.path.exists(assets_path1) == False:
+	copy_mks_assets1()
 
 if os.path.exists(zip_path) == False:
 	download_mks_assets()
-	download_mks_assets1()
 
 if os.path.exists(assets_path) == False:
 	copy_mks_assets()
-	copy_mks_assets1()
